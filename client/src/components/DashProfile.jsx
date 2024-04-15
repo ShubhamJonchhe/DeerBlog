7e04b07e34a72870ed1c374573f25b121c13e4cf
Shubham <aaronjonchhe@gmail.com>
2024-04-16 00:59:24 +0545

    Complete create post page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -8,9 +8,10 @@ import 'react-circular-progressbar/dist/styles.css';
 import { updateStart, updateFailure, updateSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess, signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-  const {currentUser, error} = useSelector((state) => state.user)
+  const {currentUser, error, loading} = useSelector((state) => state.user)
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -160,7 +161,18 @@ export default function DashProfile() {
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange}/>
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
         <TextInput type='password' id='password' placeholder='Password' onChange={handleChange}/>
-        <Button type='submit' gradientDuoTone='greenToBlue'> Update </Button>
+        <Button type='submit' gradientDuoTone='greenToBlue' disabled={loading || imageFileUploading}> 
+          {loading ? 'loading...' : 'Update'}
+        </Button>
+        {
+          currentUser.isAdmin && (
+            <Link to={'/create-post'}>
+            <Button type='button' gradientDuoTone='purpleToPink' className='w-full'>
+              Create a post
+            </Button>
+            </Link>
+          )
+        }
       </form>
       <div className='text-red-500 flex justify-between my-5'>
         <span onClick={()=>setShowModal(true)} className='cursor-pointer'> Delete Account</span>
